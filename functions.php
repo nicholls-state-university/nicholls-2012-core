89286fe99f4f5b4194d588e8e31e90cd06f46e64
Nicholls Website Manager <nichweb@nicholls.edu>
2010-11-03 20:20:48 +0000

    Fix new path names

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define theme core file location and uri
-define( NICHOLLS_CORE_DIR, WP_CONTENT_DIR . '/themes/nicholls-core' );
-define( NICHOLLS_CORE_URL, WP_CONTENT_URL . '/themes/nicholls-core' );
+define( NICHOLLS_CORE_DIR, WP_CONTENT_DIR . '/themes/nicholls-2011-core' );
+define( NICHOLLS_CORE_URL, WP_CONTENT_URL . '/themes/nicholls-2011-core' );
 
 // Include core functions, actions, and filters.
 require_once( NICHOLLS_CORE_DIR . '/library/php/functions.php' );
