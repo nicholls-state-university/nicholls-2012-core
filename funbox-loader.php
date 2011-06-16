3a2dad787f2ebebcf543f1d58171163e228674e3
Nicholls Website Manager <nichweb@nicholls.edu>
2011-06-16 21:58:30 +0000

    Nicholls 2011 Core theme, move theme feature and custom header support to core functions.php file so it can be used by sub-themes.

--- funbox-loader.php
+++ funbox-loader.php
@@ -1,40 +1,6 @@
 <?php
 
 /*
-* Funbox Theme Support Filter
-*
-* Since this may be delpolyed on WordPress sites lacking some theme support features.
-*
-* @since 1.0
-*/
-function ftf_theme_support_filter( $features ) {
-	// Set and filter WordPress theme support features
-	$features['post-formats'] = false;
-	$features['custom-background'] = false;
-	return $features;
-}
-add_filter( 'ftf_theme_support', 'ftf_theme_support_filter' );
-
-/*
-* Funbox Theme Custom Header Filter
-*
-* Modifies the output for custom header css.
-*
-* @since 1.0
-*/
-function ftf_theme_custom_header_filter( $custom_header ) {
-	// Set and filter WordPress theme support features
-	$custom_header['no_header_text'] = false;
-	$custom_header['css_name'] = '.header-nicholls-';
-	$custom_header['header_image'] = NICHOLLS_CORE_URL . '/library/images/backgrounds/bg-1.jpg';
-	$custom_header['header_image_width'] = 962;
-	$custom_header['header_image_width'] = 158;
-	$custom_header['css_position_x'] = right;
-	return $custom_header;
-}
-add_filter( 'ftf_custom_header', 'ftf_theme_custom_header_filter' );
-
-/*
 * Funbox Shut Up Theme Default Actions
 *
 * Default actions for the Funbox Theme Framework are added by the ftf_init
