f7042127ecb2f64f7cd10a6595ba4925d1090c9a
Nicholls Website Manager <nichweb@nicholls.edu>
2012-02-09 21:18:08 +0000

    OOPS. Too many of these today. Remember your Bash history might just shoot you in the ass.

--- nicholls-2011-core/megamenu/megamenu-template-custom-post-type.php
+++
@@ -1,8 +0,0 @@
-<?php @header('Content-Type: text/html; charset=' . get_option('blog_charset')); ?>
-
-<?php while ( have_posts() ) : the_post() ?>
-		
-	<?php the_content(); ?>
-				
-<?php endwhile; ?>
-
