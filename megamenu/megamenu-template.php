90d9bec1a078ff922c60553bec9b0fe278732394
Nicholls Website Manager <nichweb@nicholls.edu>
2012-11-30 22:08:43 +0000

    Nicholls 2012 Core theme - get rid of the title attributes.

--- megamenu/megamenu-template.php
+++ megamenu/megamenu-template.php
@@ -19,33 +19,33 @@
 				<div class="nicholls-menu-contents" id="nicholls-menu-current-contents">
 					<h2 class="nicholls-menu-title-">Current Students</h2>
 					<div class="column- column-1">
-						<h3>On-line</h3>
+						<h3>Online Services</h3>
 						<ul>
-							<li><a title="Student E-mail" href="http://www.nicholls.edu/student-email/">Student E-mail</a></li>
-							<li><a title="Banner" href="http://www.nicholls.edu/banner/">Banner</a></li>
-							<li><a title="Moodle" href="http://moodle.nicholls.edu/moodle/">Moodle</a></li>
-							<li><a title="Calendar" href="http://www.nicholls.edu/calendar/">Calendar</a></li>
-							<li><a title="Request Transcripts" href="http://www.nicholls.edu/records/transcripts/">Request Transcripts</a></li>
+							<li><a href="http://www.nicholls.edu/student-email/">Student E-mail</a></li>
+							<li><a href="http://www.nicholls.edu/banner/">Banner</a></li>
+							<li><a href="http://moodle.nicholls.edu/moodle/">Moodle</a></li>
+							<li><a href="http://www.nicholls.edu/calendar/">Calendar</a></li>
+							<li><a href="http://www.nicholls.edu/records/transcripts/">Request Transcripts</a></li>
 						</ul>
 						<h3>Resources</h3>
 						<ul>
-							<li><a title="Housing" href="http://www.nicholls.edu/housing/">Housing</a></li>
-							<li><a title="Meal Plans &amp; Dining" href="http://www.nicholls.edu/dining/">Meal Plans &amp; Dining</a></li>
-							<li><a title="Colonel Card (ID)" href="http://www.nicholls.edu/colonelcard/">Colonel Card (ID)</a></li>
-							<li><a title="Bookstore" href="http://nich.bkstore.com/ ">Bookstore</a></li>
-							<li><a title="Library" href="http://www.nicholls.edu/library/">Library</a></li>
-							<li><a title="Health Services" href="http://www.nicholls.edu/health/">Health Services</a></li>
+							<li><a href="http://www.nicholls.edu/housing/">Housing</a></li>
+							<li><a href="http://www.nicholls.edu/dining/">Meal Plans &amp; Dining</a></li>
+							<li><a href="http://www.nicholls.edu/colonelcard/">Colonel Card (ID)</a></li>
+							<li><a href="http://nich.bkstore.com/ ">Bookstore</a></li>
+							<li><a href="http://www.nicholls.edu/library/">Library</a></li>
+							<li><a href="http://www.nicholls.edu/health/">Health Services</a></li>
 						</ul>						
 					</div>
 					<div class="column- column-2">			
 						<h3>Information</h3>
 						<ul>
-							<li><a title="Faculty &amp; Staff Directory" href="http://www.nicholls.edu/directory/">Faculty &amp; Staff Directory</a></li>
-							<li><a title="Registration/Schedule of Classes" href="http://www.nicholls.edu/register/">Registration/Schedule of Classes</a></li>
-							<li><a title="Catalog" href="http://www.nicholls.edu/catalog/">Catalog</a></li>
-							<li><a title="Majors, Degrees &amp; Programs of Study" href="http://www.nicholls.edu/programs/">Majors, Degrees &amp; Programs of Study</a></li>
-							<li><a title="Fee Payment / Fee Information" href="http://www.nicholls.edu/fees/">Fee Payment / Fee Information</a></li>
-							<li><a title="Financial Aid &amp; Scholarships" href="http://www.nicholls.edu/financial-aid/">Financial Aid &amp; Scholarships</a></li>
+							<li><a href="http://www.nicholls.edu/directory/">Faculty &amp; Staff Directory</a></li>
+							<li><a href="http://www.nicholls.edu/register/">Registration/Schedule of Classes</a></li>
+							<li><a href="http://www.nicholls.edu/catalog/">Catalog</a></li>
+							<li><a href="http://www.nicholls.edu/programs/">Majors, Degrees &amp; Programs of Study</a></li>
+							<li><a href="http://www.nicholls.edu/fees/">Fee Payment / Fee Information</a></li>
+							<li><a href="http://www.nicholls.edu/financial-aid/">Financial Aid &amp; Scholarships</a></li>
 						</ul>
 					</div>
 				</div>				
@@ -59,27 +59,27 @@
 					<div class="column- column-1">
 						<h3>Applying to Nicholls</h3>						
 						<ul>
-							<li><a title="Request Admission Information" href="http://www.nicholls.edu/request_info/">Request Admission Information</a></li>
-							<li><a title="Majors, Degrees &amp; Programs of Study" href="http://www.nicholls.edu/programs/">Majors, Degrees &amp; Programs of Study</a></li>
-							<li><a title="Campus Tours" href="http://www.nicholls.edu/tours/">Campus Tours</a></li>
-							<li><a title="Fee Payment / Fee Information" href="http://www.nicholls.edu/fees/">Fee Payment / Fee Information</a></li>
-							<li><a title="Financial Aid &amp; Scholarships" href="http://www.nicholls.edu/financial-aid/">Financial Aid &amp; Scholarships</a></li>
+							<li><a href="http://www.nicholls.edu/request_info/">Request Admission Information</a></li>
+							<li><a href="http://www.nicholls.edu/programs/">Majors, Degrees &amp; Programs of Study</a></li>
+							<li><a href="http://www.nicholls.edu/tours/">Campus Tours</a></li>
+							<li><a href="http://www.nicholls.edu/fees/">Fee Payment / Fee Information</a></li>
+							<li><a href="http://www.nicholls.edu/financial-aid/">Financial Aid &amp; Scholarships</a></li>
 						</ul>
 					</div>
 					<div class="column- column-2">
 						<h3>More Information</h3>						
 						<ul>
-							<li><a title="Office of Admissions" href="http://www.nicholls.edu/admission/">Office of Admissions</a></li>
-							<li><a title="Transfer Students" href="http://www.nicholls.edu/transfer/">Transfer Students</a></li>
-							<li><a title="Veterans" href="http://www.nicholls.edu/veterans/">Veterans</a></li>
-							<li><a title="Academics &amp; Testing" href="http://www.nicholls.edu/academics/">Academics &amp; Testing</a></li>
-							<li><a title="Orientation" href="http://www.nicholls.edu/orientation/">Orientation</a></li>
-							<li><a title="Nicholls Online" href="http://www.nicholls.edu/nicholls-online/">Nicholls Online</a></li>
+							<li><a href="http://www.nicholls.edu/admission/">Office of Admissions</a></li>
+							<li><a href="http://www.nicholls.edu/transfer/">Transfer Students</a></li>
+							<li><a href="http://www.nicholls.edu/veterans/">Veterans</a></li>
+							<li><a href="http://www.nicholls.edu/academics/">Academics &amp; Testing</a></li>
+							<li><a href="http://www.nicholls.edu/orientation/">Orientation</a></li>
+							<li><a href="http://www.nicholls.edu/nicholls-online/">Nicholls Online</a></li>
 						</ul>
 						<h3>Services</h3>						
 						<ul>
-							<li><a title="Housing" href="http://www.nicholls.edu/housing/">Housing</a></li>
-							<li><a title="Meal Plans &amp; Dining" href="http://www.nicholls.edu/dining/">Meal Plans &amp; Dining</a></li>
+							<li><a href="http://www.nicholls.edu/housing/">Housing</a></li>
+							<li><a href="http://www.nicholls.edu/dining/">Meal Plans &amp; Dining</a></li>
 						</ul>						
 					</div>					
 				</div>			
@@ -91,32 +91,32 @@
 				<div class="nicholls-menu-contents" id="nicholls-menu-life-contents">
 					<h2 class="nicholls-menu-title-">Housing &amp; Campus Life</h2>
 					<div class="column- column-1">
-						<h3>On-line</h3>
+						<h3>Online Services</h3>
 						<ul>
-							<li><a title="Calendar" href="http://www.nicholls.edu/calendar/">Calendar</a></li>
-							<li><a title="Maintenance Request" href="http://www.nicholls.edu/housing/online-maintenance-request/">Maintenance Request</a></li>
-							<li><a title="Reserve Campus Facility" href="http://www.nicholls.edu/union/reservations/">Reserve Campus Facility</a></li>
+							<li><a href="http://www.nicholls.edu/calendar/">Calendar</a></li>
+							<li><a href="http://www.nicholls.edu/housing/online-maintenance-request/">Maintenance Request</a></li>
+							<li><a href="http://www.nicholls.edu/union/reservations/">Reserve Campus Facility</a></li>
 						</ul>
 						<h3>Campus Life</h3>
 						<ul>
-							<li><a title="Student Government Association (SGA)" href="http://www.nicholls.edu/sga/">Student Government Association (SGA)</a></li>
-							<li><a title="Student Organizations" href="http://www.nicholls.edu/organizations/">Student Organizations</a></li>
-							<li><a title="Greek Life" href="http://www.nicholls.edu/greek/">Greek Life</a></li>
-							<li><a title="The Nicholls Worth" href="http://www.thenichollsworth.com/">The Nicholls Worth</a></li>
+							<li><a href="http://www.nicholls.edu/sga/">Student Government Association (SGA)</a></li>
+							<li><a href="http://www.nicholls.edu/organizations/">Student Organizations</a></li>
+							<li><a href="http://www.nicholls.edu/greek/">Greek Life</a></li>
+							<li><a href="http://www.thenichollsworth.com/">The Nicholls Worth</a></li>
 						</ul>						
 					</div>
 					<div class="column- column-2">
 						<h3>Resources</h3>
 						<ul>
-							<li><a title="Student Union" href="http://www.nicholls.edu/union/">Student Union</a></li>
-							<li><a title="Colonel Card (ID)" href="http://www.nicholls.edu/colonelcard/">Colonel Card (ID)</a></li>
-							<li><a title="Housing" href="http://www.nicholls.edu/housing/">Housing</a></li>
-							<li><a title="Meal Plans &amp; Dining" href="http://www.nicholls.edu/dining/">Meal Plans &amp; Dining</a></li>
-							<li><a title="Parking Services" href="http://www.nicholls.edu/parking/">Parking Services</a></li>
-							<li><a title="Student Services" href="http://www.nicholls.edu/services/">Student Services</a></li>
-							<li><a title="Health Services" href="http://www.nicholls.edu/health/">Health Services</a></li>
-							<li><a title="Campus Recreation" href="http://www.nicholls.edu/recreation/">Campus Recreation</a></li>
-							<li><a title="Bookstore" href="http://nich.bkstore.com/">Bookstore</a></li>							
+							<li><a href="http://www.nicholls.edu/union/">Student Union</a></li>
+							<li><a href="http://www.nicholls.edu/colonelcard/">Colonel Card (ID)</a></li>
+							<li><a href="http://www.nicholls.edu/housing/">Housing</a></li>
+							<li><a href="http://www.nicholls.edu/dining/">Meal Plans &amp; Dining</a></li>
+							<li><a href="http://www.nicholls.edu/parking/">Parking Services</a></li>
+							<li><a href="http://www.nicholls.edu/services/">Student Services</a></li>
+							<li><a href="http://www.nicholls.edu/health/">Health Services</a></li>
+							<li><a href="http://www.nicholls.edu/recreation/">Campus Recreation</a></li>
+							<li><a href="http://nich.bkstore.com/">Bookstore</a></li>							
 						</ul>
 					</div>
 				</div>
@@ -133,29 +133,30 @@
 				<div class="nicholls-menu-contents" id="nicholls-menu-friends-contents">
 					<h2 class="nicholls-menu-title-">Alumni &amp; Friends</h2>
 					<div class="column- column-1">
-						<h3>On-line</h3>
+						<h3>Online Services</h3>
 						<ul>
-							<li><a title="Calendar" href="http://www.nicholls.edu/calendar/">Calendar</a></li>
-							<li><a title="Request Transcripts" href="http://www.nicholls.edu/records/transcripts/">Request Transcripts</a></li>
+							<li><a href="http://www.nicholls.edu/calendar/">Calendar</a></li>
+							<li><a href="http://www.nicholls.edu/records/transcripts/">Request Transcripts</a></li>
 						</ul>					
-						<h3>Give Back</h3>
+						<h3>Get Connected</h3>
 						<ul>
-							<li><a title="Nicholls Alumni Federation" href="http://www.nichollsalumni.org/">Nicholls Alumni Federation</a></li>
-							<li><a title="Colonel Athletic Association" href="http://www.geauxcolonels.com/sb_output.aspx?form=6&">Colonel Athletic Association</a></li>
+							<li><a href="http://www.nicholls.edu/development/giving-to-nicholls/">Nicholls Foundation</a></li>
+							<li><a href="http://www.nichollsalumni.org/">Nicholls Alumni Federation</a></li>
+							<li><a href="http://www.geauxcolonels.com/sb_output.aspx?form=6&">Colonel Athletic Association</a></li>
 						</ul>							
 					</div>
 					<div class="column- column-2">
 						<h3>Information</h3>
 						<ul>
-							<li><a title="Distance Education" href="http://www.nicholls.edu/distance/">Distance Education</a></li>
-							<li><a title="Continuing Education" href="http://www.nicholls.edu/continuing-ed/">Continuing Education</a></li>
-							<li><a title="Nicholls Online" href="http://www.nicholls.edu/nicholls-online/">Nicholls Online</a></li>
+							<li><a href="http://www.nicholls.edu/distance/">Distance Education</a></li>
+							<li><a href="http://www.nicholls.edu/continuing-ed/">Continuing Education</a></li>
+							<li><a href="http://www.nicholls.edu/nicholls-online/">Nicholls Online</a></li>
 						</ul>
 						<h3>Nicholls Publications</h3>
 						<ul>					
-							<li><a title="The Colonel" href="http://www.nichollsalumni.org/the-colonel/">The Colonel</a></li>
-							<li><a title="Voila" href="http://www.nicholls.edu/voila/">Voila</a></li>
-							<li><a title="Mosaic" href="http://www.nicholls.edu/mosaic/">Mosaic</a></li>
+							<li><a href="http://www.nichollsalumni.org/the-colonel/">The Colonel</a></li>
+							<li><a href="http://www.nicholls.edu/voila/">Voila</a></li>
+							<li><a href="http://www.nicholls.edu/mosaic/">Mosaic</a></li>
 						</ul>						
 					</div>
 				</div>				
@@ -167,28 +168,28 @@
 				<div class="nicholls-menu-contents" id="nicholls-menu-administration-contents">
 					<h2 class="nicholls-menu-title-">Faculty &amp; Staff</h2>
 					<div class="column- column-1">
-						<h3>On-line</h3>
+						<h3>Online Services</h3>
 						<ul>
-							<li><a title="Faculty &amp; Staff E-mail" href="https://www.google.com/a/nicholls.edu/#inbox">Faculty &amp; Staff E-mail</a></li>
-							<li><a title="Moodle" href="http://moodle.nicholls.edu/moodle/">Moodle</a></li>
-							<li><a title="Employee Self Service / Banner" href="https://talbot.nicholls.edu:4538/PROD/twbkwbis.P_GenMenu?name=homepage">Employee Self Service / Banner</a></li>
-							<li><a title="Faculty &amp; Staff Directory" href="http://www.nicholls.edu/directory/">Faculty &amp; Staff Directory</a></li>
-							<li><a title="Calendar" href="http://www.nicholls.edu/calendar/">Calendar</a></li>
+							<li><a href="https://www.google.com/a/nicholls.edu/#inbox">Faculty &amp; Staff E-mail</a></li>
+							<li><a href="http://moodle.nicholls.edu/moodle/">Moodle</a></li>
+							<li><a href="https://talbot.nicholls.edu:4538/PROD/twbkwbis.P_GenMenu?name=homepage">Employee Self Service / Banner</a></li>
+							<li><a href="http://www.nicholls.edu/directory/">Faculty &amp; Staff Directory</a></li>
+							<li><a href="http://www.nicholls.edu/calendar/">Calendar</a></li>
 						</ul>
 						<h3>Information</h3>
 						<ul>
-							<li><a title="University Administration" href="http://www.nicholls.edu/administration/">University Administration</a></li>
-							<li><a title="Policies &amp; Procedures" href="http://www.nicholls.edu/administration/university-policies-procedures/">Policies &amp; Procedures</a></li>
-							<li><a title="Employee Holiday Schedule" href="http://www.nicholls.edu/administration/university-administrative-holiday-schedule/">Employee Holiday Schedule</a></li>
+							<li><a href="http://www.nicholls.edu/administration/">University Administration</a></li>
+							<li><a href="http://www.nicholls.edu/administration/university-policies-procedures/">Policies &amp; Procedures</a></li>
+							<li><a href="http://www.nicholls.edu/administration/university-administrative-holiday-schedule/">Employee Holiday Schedule</a></li>
 						</ul>						
 					</div>
 					<div class="column- column-2">
 						<h3>Resources</h3>
 						<ul>
-							<li><a title="Faculty Senate" href="http://www.nicholls.edu/faculty-senate/">Faculty Senate</a></li>
-							<li><a title="Center for Advancing Faculty Engagement (CAFE)" href="http://www.nicholls.edu/cafe/">Center for Advancing Faculty Engagement (CAFE)</a></li>
-							<li><a title="Human Resources" href="http://www.nicholls.edu/human-resources/">Human Resources</a></li>
-							<li><a title="Employment Opportunities" href="https://jobs.nicholls.edu">Employment Opportunities</a></li>
+							<li><a href="http://www.nicholls.edu/faculty-senate/">Faculty Senate</a></li>
+							<li><a href="http://www.nicholls.edu/cafe/">Center for Advancing Faculty Engagement (CAFE)</a></li>
+							<li><a href="http://www.nicholls.edu/human-resources/">Human Resources</a></li>
+							<li><a href="https://jobs.nicholls.edu">Employment Opportunities</a></li>
 						</ul>						
 					</div>
 				</div>
