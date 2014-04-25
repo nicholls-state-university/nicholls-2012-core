49e63e8d82cf9948bd0ce207ef6c32c0f1aca6a7
Nicholls Website Manager <nichweb@nicholls.edu>
2014-04-25 18:30:16 +0000

    Nicholls 2012 Core theme, updated google analytics tracking JS code.

--- functions.php
+++ functions.php
@@ -230,17 +230,14 @@ add_action( 'nicholls_header_end', 'nicholls_html_go_to', 99 );
 */
 function nicholls_js_google_analytics() {
 ?>
-<script type="text/javascript">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-23854260-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  ga('create', 'UA-23854260-2', 'nicholls.edu');
+  ga('send', 'pageview');
 
 </script>
 <?php
