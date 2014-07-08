f4aa04b878091cead61026d238990827887071b0
Nicholls Website Manager <nichweb@nicholls.edu>
2014-07-08 19:43:53 +0000

    Nicholls 2012 Core theme - upgrade Compass, Susy, etc. Refactor some CSS improve Emergency Notices.

--- library/source/compass/config.rb
+++ library/source/compass/config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
 # Susy Grids
+require 'breakpoint'
 require 'susy'
 # CSS Normalize
 require 'compass-normalize'
@@ -19,6 +20,7 @@ images_dir = "library/images"
 javascripts_dir = "js"
 project_type = :stand_alone
 output_style = :compressed
+#output_style = :expanded
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -36,27 +38,53 @@ output_style = :compressed
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 
+asset_cache_buster :none
+
+# Make a copy of sprites with a name that has no uniqueness of the hash.
+on_sprite_saved do |filename|
+  if File.exists?(filename)    
+    FileUtils.mv filename, filename.gsub(%r{-s[a-z0-9]{10}\.png}, '.png').gsub('source/compass/library/', '')
+  end
+end
+
+# Replace in stylesheets generated references to sprites
+# by their counterparts without the hash uniqueness.
+on_stylesheet_saved do |filename|
+  if File.exists?(filename)
+    css = File.read filename
+    File.open(filename, 'w+') do |f|
+      f << css.gsub(%r{-s([a-z0-9]{10})\.png}, '.png?v\1').gsub('source/compass/library/', '')
+    end
+  end
+end
+
+# Rename sprites to remove the Compass-generated hash and move it up 1 directory
+#on_sprite_saved do |filename|
+#	if File.exists?(filename)
+#		FileUtils.mv filename, filename.gsub(%r{-s[a-z0-9]{10}\.}, '.').gsub('images/../images/', '')
+#	end
+#end
 
 # rename_sprite module from comments at http://www.akawebdesign.com/2011/01/17/building-css-sprites-with-compasssass/
-module Sass::Script::Functions
-	require 'fileutils'
-	
-	def rename_sprite(sprite_map, target_relative)
-		
-		target_relative = target_relative.to_s
-		target_relative.gsub!(/.*images\//,'')
-		target_relative.gsub!(/"$/,'')
-		sprite_map = sprite_map.to_s
-		sprite_map["url('"] = ""
-		sprite_map["')"] = ""
-		
-		sass_absolute_path = File.dirname(__FILE__)
-		sprite_absolute_path= File.join(sass_absolute_path, "library/images/sprites", File.basename(sprite_map))
-		target_absolute_path= File.join(sass_absolute_path, "images", target_relative)
-		
-		target_manual = File.join( Compass.configuration.css_path, "library/images/", target_relative)
-		
-		FileUtils.mv(sprite_absolute_path, target_manual)
-		return Sass::Script::Bool.new(true)
-	end
-end
+#module Sass::Script::Functions
+#	require 'fileutils'
+#	
+#	def rename_sprite(sprite_map, target_relative)
+#		
+#		target_relative = target_relative.to_s
+#		target_relative.gsub!(/.*images\//,'')
+#		target_relative.gsub!(/"$/,'')
+#		sprite_map = sprite_map.to_s
+#		sprite_map["url('"] = ""
+#		sprite_map["')"] = ""
+#		
+#		sass_absolute_path = File.dirname(__FILE__)
+#		sprite_absolute_path= File.join(sass_absolute_path, "library/images/sprites", File.basename(sprite_map))
+#		target_absolute_path= File.join(sass_absolute_path, "images", target_relative)
+#		
+#		target_manual = File.join( Compass.configuration.css_path, "library/images/", target_relative)
+#		
+#		FileUtils.mv(sprite_absolute_path, target_manual)
+#		return Sass::Script::Bool.new(true)
+#	end
+#end
